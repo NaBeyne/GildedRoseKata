09e4dbf99acbbaa770e14024e6b689e60aeaee5d
NathanBeyne <nathan.beyne@iqgeo.com>
2024-03-08 17:10:35 +0100

    Remove duplicated code by creating methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -5,6 +5,8 @@ class GildedRose {
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -14,22 +16,21 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if(!item.name.equals(SULFURAS)) {
-                if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals(BACKSTAGE_PASSES)) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
+                if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
+                    if (isQualityHigherThanMinValue(item)) {
+                        decreaseQuality(item);
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                    if (isQualityLowerThanMaxValue(item)) {
+                        increaseQuality(item);
 
-                        if (item.name.equals(BACKSTAGE_PASSES)) {
-                            if (item.sellIn < 11 && item.quality < 50) {
-                                item.quality = item.quality + 1;
+                        if (item.name.equals(BACKSTAGE_PASSES) && isQualityLowerThanMaxValue(item)) {
+                            if (item.sellIn < 11) {
+                                increaseQuality(item);
                             }
 
-                            if (item.sellIn < 6 && item.quality < 50) {
-                                item.quality = item.quality + 1;
+                            if (item.sellIn < 6) {
+                                increaseQuality(item);
                             }
                         }
                     }
@@ -37,22 +38,42 @@ class GildedRose {
 
                 item.sellIn = item.sellIn - 1;
 
-                if (item.sellIn < 0) {
+                if (isExpired(item)) {
                     if (!item.name.equals(AGED_BRIE)) {
                         if (!item.name.equals(BACKSTAGE_PASSES)) {
-                            if (item.quality > 0) {
-                                item.quality = item.quality - 1;
+                            if (isQualityHigherThanMinValue(item)) {
+                                decreaseQuality(item);
                             }
                         } else {
                             item.quality = 0;
                         }
                     } else {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                        if (isQualityLowerThanMaxValue(item)) {
+                            increaseQuality(item);
                         }
                     }
                 }
             }
         }
+    }
+
+    private boolean isQualityHigherThanMinValue(Item item) {
+        return item.quality > MIN_QUALITY;
+    }
+
+    private boolean isQualityLowerThanMaxValue(Item item) {
+        return item.quality < MAX_QUALITY;
+    }
+
+    private boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private void increaseQuality(Item item) {
+        item.quality++;
+    }
+
+    private void decreaseQuality(Item item) {
+        item.quality--;
     }
 }
